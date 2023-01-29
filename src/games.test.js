54086243c2f6b759fa837b86f18633d6559d7ded
Jesus <jesus.cassano.2006@gmail.com>
2023-01-29 19:57:17 +0100

    add testing

--- src/games.test.js
+++ src/games.test.js
@@ -6,5 +6,13 @@ describe("Given an array of length 9", () => {
       const result = countNeighbor(0, 0);
       expect(result).toBe(2);
     });
+    test("Should be a neighbor count case 2 ", () => {
+      const result = countNeighbor(1, 1);
+      expect(result).toBe(3);
+    });
+    test("Should be a neighbor cont case 3 ", () => {
+      const result = countNeighbor(0, 1);
+      expect(result).toBe(1);
+    });
   });
 });
