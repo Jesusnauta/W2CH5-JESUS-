65fb1c1807b01234e7aba66c18aa4ce9c7c19ea7
Jesus <jesus.cassano.2006@gmail.com>
2023-01-29 19:20:41 +0100

    add firts test

--- games.test.js
+++ games.test.js
@@ -3,14 +3,8 @@ import { countNeighbor } from "./games";
 describe("Given an array of length 9", () => {
   describe("When we call countNeighbor function", () => {
     test("Should be a neighbor count ", () => {
-      const array = [
-        [0, 1, 0],
-        [1, 0, 0],
-        [1, 0, 0],
-      ];
-
-      const result = countNeighbor(array[0], array[0][0]);
-      expect(result).toHaveBeenCalled();
+      const result = countNeighbor(0, 0);
+      expect(result).toBe(2);
     });
   });
 });
